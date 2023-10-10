2094263479a7b90274c9997394a71cc62e4096d3
WolferWolfy <wolferwolfy@gmail.com>
2023-10-10 17:34:24 +0200

    Add items to category

--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -4,10 +4,12 @@ public struct GroceryCategoryResponseDTO: Codable {
 	public let id: UUID
 	public let title: String
 	public let color: String
+	public let items: [GroceryItemResponseDTO]
 	
-	public init(id: UUID, title: String, color: String) {
+	public init(id: UUID, title: String, color: String, items: [GroceryItemResponseDTO] = []) {
 		self.id = id
 		self.title = title
 		self.color = color
+		self.items = items
 	}
 }
